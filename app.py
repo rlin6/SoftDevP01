3743ea324b8c2745b332d0f3c9d861ce64734fdd
rlin6 <rlin6@stuy.edu>
2018-11-26 12:35:14 -0500

    working on app login

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
-from flask import Flask, render_template
+#BooStRadley - Ricky Lin, Matthew Ming, Mohammed Uddin, Sophia Xia
+
+from flask import Flask, render_template, session, redirect
+#import os 
 
 app = Flask(__name__)
 
+#app.secret_key = os.urandom(32)
+
 @app.route("/")
-def home():
-    print(app)
+def index():
+
+    if 'user' in session:
+        return redirect('/track')
+    
     return render_template("index.html")
 
 @app.route("/signup")
 def signup():
+
+    if 'user' in session:
+        return redirect('/track')
+    
     return render_template("signup.html")
+
+@app.route("/auth", methods = ['POST','GET'])
+def auth():
+
+    message = ''
+
+    if request.method == 'GET' or not ('user' in request.form.keys()):
+        return redirect('/')
+
+    if "conf" in request.form.keys():
+        message = auth.register(request.form['user'], request.form['pwd'], request.form['conf'])
+
+    else: message = auth.CheckInfo(request.form['user'], request.form['pwd'])
+
+    if message in ["Account creation successful", "Login Successful"]:
+        session['user'] = re
 
 @app.route("/track")
 def track():
